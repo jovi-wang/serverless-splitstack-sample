a887800efbf14bdf52f64407cf5752bfa127437c
jovi <jovi_wang@outlook.com>
2019-03-05 21:50:58 +1100

    update comment

--- serverless/api/v1/users/userHandler.js
+++ serverless/api/v1/users/userHandler.js
@@ -6,12 +6,16 @@ const uuid = require('uuid');
 const { errorResponse } = require('../../../helpers/helper');
 const { UserSchema } = require('../../../models');
 const dbUtil = require('../../../utils/dbUtil');
-
+// const _difference = require('lodash/difference');
+// const _dropRight = require('lodash/dropRight');
+require('lodash');
 
 
 
 module.exports.testHandler = async (event) => {
   try {
+    console.log(_difference([2, 1], [2, 3]));
+    console.log(_dropRight([1, 2, 3]));
     const dbResults = await dbUtil.getOne('User', UserSchema, {email:'abc@test.com', sub: 'sub-abc'});
     console.log(dbResults)
     return {
